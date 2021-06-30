355d2437f7899ff330a11c5b345e29bd726afbbc
dhasamithra <74002230+dhasamithra@users.noreply.github.com>
2021-06-30 19:34:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+function steup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('myFliterImage.png');
+}
